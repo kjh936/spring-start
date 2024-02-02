df096b5680fe3446d00813c977ff1a09bfe87d6d
kjh <kjh4940@naver.com>
2024-02-02 21:00:35 +0900

    no message

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,13 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class TimeTraceAop {
+
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        
+        Object result = joinPoint.proceed();
+    }
+}
